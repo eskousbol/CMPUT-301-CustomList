fd637bc78135a7b949f2106b255aef175acc5828
= <skousbol@ualberta.ca>
2021-10-27 17:24:31 -0600

    Changed notation

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.example.simpleparadox.listycity.City;
 import com.example.simpleparadox.listycity.CustomList;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,21 +11,15 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
 
-    @BeforeAll
-    public void createList(){
+    @BeforeEach
+    public void createList() {
         list = new CustomList(null, new ArrayList<City>());
     }
 
-    /**
-     * Test that add city works
-     */
     @Test
     public void addCityTest(){
         int listSize = list.getCount();
         list.addCity(new City("Halifax", "NS"));
-        assertEquals(list.getCount(), listSize + 1);
-
-
-
+        assertEquals(listSize + 1, list.getCount());
     }
 }
